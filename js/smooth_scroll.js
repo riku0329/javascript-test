0453de29e11d0b9fb2981f7de84161fec88560a3
riku0329 user name <riku03290@gmail.com>
2020-06-23 13:12:08 +0900

    create smooth scroll

--- js/smooth_scroll.js
+++ js/smooth_scroll.js
@@ -3,12 +3,28 @@ document.addEventListener(
   'DOMContentLoaded',
   function () {
     // 「window.scroll」を使ってスクロールさせましょう
-    const SPEED = 350;
+    const scrollTriggers = document.querySelectorAll('a[href^="#"]');
+    scrollTriggers.forEach((trigger) => {
+      trigger.addEventListener('click', (e) => {
+        const href = trigger.getAttribute('href');
+        const currentPos = window.pageYOffset;
+        const targetEl = document.getElementById(href.replace('#', ''));
+        // console.log(href);
+        // console.log(targetEl);
+        // console.log(currentPos);
+        if (targetEl) {
+          e.preventDefault();
+          const targetPos =
+            window.pageYOffset + targetEl.getBoundingClientRect().top - 70;
+          console.log(targetPos);
+          window.scroll({
+            top: targetPos,
+            behavior: 'smooth',
+          });
+        }
+      });
+    });
     // jQueryと違い要素一つ一つにイベントをセットしたり、値を変更したりしなければなりません
-    const scrollTrigger = document.querySelector('a[href^="#"]');
-    scrollTrigger.addEventListener('click', () => {
-      console.log('hello');
-    })
     // [JavaScript で forEach を使うのは最終手段](https://qiita.com/diescake/items/70d9b0cbd4e3d5cc6fce)
   },
   false
